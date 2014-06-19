f8aee208f97470985843ab5ab283fb13525d8774
Hassan46 <ihassan464@gmail.com>
2014-06-19 21:43:08 +0500

    Update single.php

--- single.php
+++ single.php
@@ -5,7 +5,12 @@ get_header(); ?>
 		<div id="content" class="site-content" role="main">
 			<?php
 				while ( have_posts() ) : the_post();
+				echo '<h5 align="center">Category: ';
+				echo get_the_category_list( _x( ', ', 'Used between list items, there is a space after the comma.', 'twentyfourteen' ) );
+				echo' <br/>';the_tags();
+				echo '</h5>';
 				echo'<h1 align="center">';the_title();echo"</h1>";
+				echo'<h5 align="center">';twentyfourteen_posted_on();echo'</h5>';
 				echo'<h4 align="center">';the_content();echo'</h4>';
 				echo'<br/><br/>';
 					if ( comments_open() || get_comments_number() ) {
